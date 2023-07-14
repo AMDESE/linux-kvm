9788b1887b70a232381603e93d6c2eca7392dca4
Manali Shukla <manali.shukla@amd.com>
2023-07-14 08:18:27 +0000

    KVM: x86: Extend CPUID range to include new leaf
    CPUID leaf 0x8000001b (EAX) provides information about Instruction-Based
    sampling capabilities on AMD Platforms.
    
    Signed-off-by: Manali Shukla <manali.shukla@amd.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -1845,6 +1845,13 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		entry->eax = entry->ebx = entry->ecx = 0;
 		entry->edx = 0; /* reserved */
 		break;
+	/* AMD IBS capability */
+	case 0x8000001B:
+		if (!kvm_cpu_cap_has(X86_FEATURE_IBS))
+			entry->eax = 0;
+
+		entry->ebx = entry->ecx = entry->edx = 0;
+		break;
 	case 0x8000001F:
 		if (!kvm_cpu_cap_has(X86_FEATURE_SEV)) {
 			entry->eax = entry->ebx = entry->ecx = entry->edx = 0;
