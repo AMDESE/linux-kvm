ccf79529bf7110c63e4b54fd3281bace2774d456
Kishon Vijay Abraham I <kvijayab@amd.com>
2024-01-25 09:00:38 +0000

    KVM: SVM/SEV: Secure AVIC: Enable NMI support
    Secure AVIC in addition to following vNMI flow for injecting NMI
    interrupts to the guest, requires V_NMI_ENABLE in VINTR_CTRL field of
    VMSA to be set. Set V_NMI_ENABLE in VINTR_CTRL field of VMSA here.
    
    Signed-off-by: Kishon Vijay Abraham I <kvijayab@amd.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -859,7 +859,7 @@ static int sev_es_sync_vmsa(struct vcpu_svm *svm)
 
 	save->sev_features = sev->vmsa_features;
 	if (svm->vcpu.arch.apic->secure_avic_active)
-		save->vintr_ctrl |= V_GIF_MASK;
+		save->vintr_ctrl |= V_GIF_MASK | V_NMI_ENABLE_MASK;
 
 	/*
 	 * Skip FPU and AVX setup with KVM_SEV_ES_INIT to avoid
