c9a23dc72762229647efb9a938dcca3a7c9a7ab7
Kishon Vijay Abraham I <kvijayab@amd.com>
2024-02-20 09:43:49 +0000

    KVM: SVM/SEV: Secure AVIC: Set VGIF in VMSA area
    Secure AVIC requires VGIF in VMSA to be set in order for interrupts to
    be delivered to the guest. Usually for enabling GIF, VGIF field (bit 9
    of VINTR_CTRL in VMCB) is set. However Secure AVIC ignores the VGIF
    field in VMCB and requires VGIF field in VINTR_CTRL of VMSA to be set.
    
    Signed-off-by: Kishon Vijay Abraham I <kvijayab@amd.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -858,6 +858,8 @@ static int sev_es_sync_vmsa(struct vcpu_svm *svm)
 	save->dr6  = svm->vcpu.arch.dr6;
 
 	save->sev_features = sev->vmsa_features;
+	if (svm->vcpu.arch.apic->secure_avic_active)
+		save->vintr_ctrl |= V_GIF_MASK;
 
 	/*
 	 * Skip FPU and AVX setup with KVM_SEV_ES_INIT to avoid
