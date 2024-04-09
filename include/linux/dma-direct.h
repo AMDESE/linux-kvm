d8d21ca947a4b99592e77ba2013ee7bf60389ffb
Alexey Kardashevskiy <aik@amd.com>
2024-04-09 12:14:02 +1000

    sev-guest: Stop changing encrypted page state for TDISP devices
    And "sev-guest: Disable SWIOTLB for TIO device's dma_map".
    
    And other things to make secure DMA work.
    Like, clear C-bit.
    And set GFP_DMA, which does not seem to matter though as down
    the stack it gets cleared anyway.
    
    CONFIG_ZONE_DMA must be off too.
    
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- include/linux/dma-direct.h
+++ include/linux/dma-direct.h
@@ -94,6 +94,10 @@ static inline dma_addr_t phys_to_dma_unencrypted(struct device *dev,
  */
 static inline dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr)
 {
+	if (dev->tdi_enabled) {
+		dev_warn_once(dev, "(TIO) Disable SME");
+		return phys_to_dma_unencrypted(dev, paddr);
+	}
 	return __sme_set(phys_to_dma_unencrypted(dev, paddr));
 }
 
