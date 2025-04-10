4aa6bd7aadcb963009c10e05c5508a94a2891c18
Alexey Kardashevskiy <aik@amd.com>
2025-04-10 12:41:41 +1000

    KVM: SEV: Share kvm_sev_info for TIO TDI_BIND operation
    Normally KVM-AMD directly calls CCP in order to communicate with the PSP.
    
    This does not work for SEV-TIO though as handling those requires KVM to
    know about passed-through devices and how those are presented to a VM;
    these are the host and guest BDFNs (PCI Bus-Device-Function).
    Instead of teaching KVM all this, KVM exits to the userspace VMM
    which knows all the bits.
    
    The SEV-TIO requests are handled as:
    
    1) KVM exits to the userspace (QEMU);
    2) QEMU calls IOMMUFD with both BDFNs, and the KVM fd;
    3) IOMMUFD gets hold of the kvm pointer;
    4) IOMMUFD calls the TSM driver (which is a part of CCP);
    5) CCP calls the PSP.
    
    Step 5) requires GCTX (an SNP VM handle) but CCP only gets a kvm pointer.
    KVM-AMD already uses the CCP symbols so it cannot export an API to get
    GCTX from the kvm pointer.
    
    Luckily the GCTX value and the kvm struct are both embedded into
    the kvm_svm struct.
    
    Share kvm_svm and kvm_sev_info between KVM-AMD and CCP to allow reading
    GCTX via a simple container_of().
    
    This should cause no behavioural change.
    
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -21,6 +21,7 @@
 
 #include <asm/svm.h>
 #include <asm/sev-common.h>
+#include <asm/sev-kvm.h>
 
 #include "cpuid.h"
 #include "kvm_cache_regs.h"
@@ -87,46 +88,6 @@ enum {
 
 /* TPR and CR2 are always written before VMRUN */
 #define VMCB_ALWAYS_DIRTY_MASK	((1U << VMCB_INTR) | (1U << VMCB_CR2))
-
-struct kvm_sev_info {
-	bool active;		/* SEV enabled guest */
-	bool es_active;		/* SEV-ES enabled guest */
-	bool need_init;		/* waiting for SEV_INIT2 */
-	unsigned int asid;	/* ASID used for this guest */
-	unsigned int handle;	/* SEV firmware handle */
-	int fd;			/* SEV device fd */
-	unsigned long policy;
-	unsigned long pages_locked; /* Number of pages locked */
-	struct list_head regions_list;  /* List of registered regions */
-	u64 ap_jump_table;	/* SEV-ES AP Jump Table address */
-	u64 vmsa_features;
-	u16 ghcb_version;	/* Highest guest GHCB protocol version allowed */
-	struct kvm *enc_context_owner; /* Owner of copied encryption context */
-	struct list_head mirror_vms; /* List of VMs mirroring */
-	struct list_head mirror_entry; /* Use as a list entry of mirrors */
-	struct misc_cg *misc_cg; /* For misc cgroup accounting */
-	atomic_t migration_in_progress;
-	void *snp_context;      /* SNP guest context page */
-	void *guest_req_buf;    /* Bounce buffer for SNP Guest Request input */
-	void *guest_resp_buf;   /* Bounce buffer for SNP Guest Request output */
-	struct mutex guest_req_mutex; /* Must acquire before using bounce buffers */
-	cpumask_var_t have_run_cpus; /* CPUs that have done VMRUN for this VM. */
-};
-
-#define SEV_POLICY_NODBG	BIT_ULL(0)
-#define SNP_POLICY_DEBUG	BIT_ULL(19)
-
-struct kvm_svm {
-	struct kvm kvm;
-
-	/* Struct members for AVIC */
-	u32 avic_vm_id;
-	u32 *avic_logical_id_table;
-	u64 *avic_physical_id_table;
-	struct hlist_node hnode;
-
-	struct kvm_sev_info sev_info;
-};
 
 struct kvm_vcpu;
 
@@ -355,11 +316,6 @@ struct svm_cpu_data {
 DECLARE_PER_CPU(struct svm_cpu_data, svm_data);
 
 void recalc_intercepts(struct vcpu_svm *svm);
-
-static __always_inline struct kvm_svm *to_kvm_svm(struct kvm *kvm)
-{
-	return container_of(kvm, struct kvm_svm, kvm);
-}
 
 static __always_inline struct kvm_sev_info *to_kvm_sev_info(struct kvm *kvm)
 {
