3e60c74bdfd182e606372a72ef47c6e5fbe21630
Ackerley Tng <ackerleytng@google.com>
2025-05-07 09:51:52 -0700

    KVM: Query guest_memfd for private/shared status
    Query guest_memfd for private/shared status if those guest_memfds
    track private/shared status.
    
    With this patch, Coco VMs can use guest_memfd for both shared and
    private memory. If Coco VMs choose to use guest_memfd for both
    shared and private memory, by creating guest_memfd with the
    GUEST_MEMFD_FLAG_MMAP flag, guest_memfd will be used to provide the
    private/shared status of the memory, instead of kvm->mem_attr_array.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    [mdr: modify to use kvm_memslot_is_gmem_only() instead of the original
     kvm_gmem_memslot_supports_shared() naming]
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2499,6 +2499,8 @@ static inline bool kvm_memslot_is_gmem_only(const struct kvm_memory_slot *slot)
 	return slot->flags & KVM_MEMSLOT_GMEM_ONLY;
 }
 
+bool kvm_gmem_is_private(struct kvm_memory_slot *slot, gfn_t gfn);
+
 #ifdef CONFIG_KVM_GENERIC_MEMORY_ATTRIBUTES
 static inline unsigned long kvm_get_memory_attributes(struct kvm *kvm, gfn_t gfn)
 {
@@ -2514,6 +2516,12 @@ bool kvm_arch_post_set_memory_attributes(struct kvm *kvm,
 
 static inline bool kvm_mem_is_private(struct kvm *kvm, gfn_t gfn)
 {
+	struct kvm_memory_slot *slot;
+
+	slot = gfn_to_memslot(kvm, gfn);
+	if (kvm_slot_has_gmem(slot) && kvm_memslot_is_gmem_only(slot))
+		return kvm_gmem_is_private(slot, gfn);
+
 	return kvm_get_memory_attributes(kvm, gfn) & KVM_MEMORY_ATTRIBUTE_PRIVATE;
 }
 #else
--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -852,6 +852,29 @@ static int kvm_gmem_mmap(struct file *file, struct vm_area_struct *vma)
 	return 0;
 }
 
+bool kvm_gmem_is_private(struct kvm_memory_slot *slot, gfn_t gfn)
+{
+	struct inode *inode;
+	struct file *file;
+	pgoff_t index;
+	bool ret;
+
+	file = kvm_gmem_get_file(slot);
+	if (!file)
+		return false;
+
+	index = kvm_gmem_get_index(slot, gfn);
+	inode = file_inode(file);
+
+	filemap_invalidate_lock_shared(inode->i_mapping);
+	ret = kvm_gmem_shareability_get(inode, index) == SHAREABILITY_GUEST;
+	filemap_invalidate_unlock_shared(inode->i_mapping);
+
+	fput(file);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(kvm_gmem_is_private);
+
 static long kvm_gmem_ioctl(struct file *file, unsigned int ioctl,
 			   unsigned long arg)
 {
