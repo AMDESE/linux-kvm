5f71bc0f08b477634e4d93d75b7111c5c5dcfd47
Ackerley Tng <ackerleytng@google.com>
2025-05-13 11:23:10 -0700

    KVM: guest_memfd: Update kvm_gmem_mapping_order to account for page status
    kvm_gmem_mapping_order() returns the maximum mapping order for a
    gfn if a page were to be faulted in for that gfn.
    
    For inodes that support a custom allocator, the maximum mapping order
    should be determined by the custom allocator in conjunction with
    guest_memfd.
    
    This patch introduces kvm_gmem_mapping_order() to take into account that
    for the guestmem_hugetlb custom allocator, pages are split if any page
    in a huge page range is shared.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    [mdr: update to drop dependency on a pre-existing
     kvm_gmem_mapping_order() stub being present since it is stubbed
     directly in KVM MMU code now]
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2535,6 +2535,7 @@ static inline bool kvm_mem_is_private(struct kvm *kvm, gfn_t gfn)
 int kvm_gmem_get_pfn(struct kvm *kvm, struct kvm_memory_slot *slot,
 		     gfn_t gfn, kvm_pfn_t *pfn, struct page **page,
 		     int *max_order);
+int kvm_gmem_mapping_order(const struct kvm_memory_slot *slot, gfn_t gfn);
 #else
 static inline int kvm_gmem_get_pfn(struct kvm *kvm,
 				   struct kvm_memory_slot *slot, gfn_t gfn,
@@ -2543,6 +2544,13 @@ static inline int kvm_gmem_get_pfn(struct kvm *kvm,
 {
 	KVM_BUG_ON(1, kvm);
 	return -EIO;
+}
+
+static inline int kvm_gmem_mapping_order(const struct kvm_memory_slot *slot,
+					 gfn_t gfn)
+{
+	BUILD_BUG();
+	return 0;
 }
 #endif /* CONFIG_KVM_GUEST_MEMFD */
 
