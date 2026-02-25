da522ab3ed87d9c2889ec2fd4197e9f23a6d0c20
Alexey Kardashevskiy <aik@amd.com>
2026-02-25 12:52:26 +1100

    pci/tsm/uapi: Add certs and such to tsm_tdisp_status
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- include/linux/pci-tsm.h
+++ include/linux/pci-tsm.h
@@ -374,11 +374,16 @@ enum tsm_tdisp_status {
 struct tsm_tdi_status {
 	__u8 status; /* enum tsm_tdisp_status */
 	__u8 state; /* enum tsm_tdisp_state */
+	__u8 meas_digest_fresh;
+	__u8 meas_digest_valid;
 	__u8 all_request_redirect;
 	__u8 bind_p2p;
 	__u8 lock_msix;
 	__u8 no_fw_update;
 	__u16 cache_line_size;
+	__u64 spdm_algos; /* Bitmask of TSM_SPDM_ALGOS */
+	__u8 certs_digest[48];
+	__u8 meas_digest[48];
 	__u8 interface_report_digest[48];
 	__u64 intf_report_counter;
 	struct tdisp_interface_id id;
